b2a4e8b95aa2b82010eaa93a3c405294a1e1b9e4
Ikwemogena Abdulai <abdulaimogena@gmail.com>
2024-06-24 00:45:26 +0100

    feat: Update GenerateJWT function to include user ID, username, and role in the JWT claims

--- internal/utils/auth/jwt.go
+++ internal/utils/auth/jwt.go
@@ -10,16 +10,20 @@ var secretKey = []byte("secret-key")
 
 type Claims struct {
     UserID int `json:"user_id"`
+	UserRole string `json:"user_role"`
+	UserUsername string `json:"user_username"`
     jwt.Claims
 }
 
-func GenerateJWT(username string) (string, error) {
+func GenerateJWT(id, username, role string) (string, error) {
 
-	token := jwt.NewWithClaims(jwt.SigningMethodHS256,
-		jwt.MapClaims{
-			"username": username,
-			"exp":      time.Now().Add(time.Hour * 24).Unix(),
-		})
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, 
+        jwt.MapClaims{ 
+        "username": username, 
+		"role": role,
+		"id": id,
+        "exp": time.Now().Add(time.Hour * 24).Unix(), 
+        })
 
 	tokenString, err := token.SignedString(secretKey)
 	if err != nil {
@@ -27,17 +31,21 @@ func GenerateJWT(username string) (string, error) {
 	}
 
 	return tokenString, nil
-
 }
 
 func VerifyToken(tokenString string) (*jwt.Token, error) {
-	token, err := jwt.ParseWithClaims(tokenString, &Claims{}, func(token *jwt.Token) (interface{}, error) {
-		return secretKey, nil
-	})
-
-	if err != nil {
-		return nil, err
-	}
-
-	return token, nil
+	
+	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+      return secretKey, nil
+   })
+  
+   if err != nil {
+      return nil, err
+   }
+  
+   if !token.Valid {
+      return nil, err
+   }
+  
+   return token, nil
 }
